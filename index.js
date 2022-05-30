03cb391fca9b99fe1b6c186ba8598cb4692a42b1
Arjun Saili <arjun7.saili@gmail.com>
2022-05-30 01:12:50 -0400

    add response status

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ app.listen(PORT);
 
 app.get('*', function(req, res){
     if(req.url == "/"){
+        res.status = 200;
         res.sendFile(path.join(__dirname, 'public', '/index.html'))
     }
     if(req.url == "/about" || req.url == "/contact-me"){
+        res.status = 200;
         res.sendFile(path.join(__dirname, 'public', `${req.url}.html`))
     }
     else{
+        res.status = 400;
         res.sendFile(path.join(__dirname, 'public', '/404.html'))
     }
 })
